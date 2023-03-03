366a30f52f5cdebb86f4e0ae67ea0d0922834d23
Muh Ibnu Habil Hanafi <ibnu.habil.h@gmail.com>
2023-03-03 14:54:03 +0700

    Add bash script to install i3wm-polybar config files

---
+++ i3wm-polybar/teal_mode_install.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+
+# Define variables
+CONFIG_DIR="/home/$USER/.config"
+POLYBAR_SCRIPTS_DIR="$CONFIG_DIR/polybar/scripts"
+XRESOURCES_FILE=".Xresources"
+XRESOURCES_DEST="/home/$USER"
+XRESOURCES_BACKUP="$XRESOURCES_DEST/$XRESOURCES_FILE-bak"
+
+# Define variables for mpv (watch youtube from mpv)
+YOUTUBE_SCRIPT="youtube-watch.sh"
+YOUTUBE_SCRIPT_SRC="./i3/scripts/$YOUTUBE_SCRIPT"
+YOUTUBE_SYMLINK_DEST="/usr/local/bin/$YOUTUBE_SCRIPT"
+
+# Check for internet connectivity
+if ! ping -q -c 1 -W 1 archlinux.org &> /dev/null
+then
+    # Display message with frame if there's no internet access
+    echo "+---------------------------------------------------------------------+"
+    echo "| ERROR: No internet connection! Please check your network settings. |"
+    echo "+---------------------------------------------------------------------+"
+    exit 1
+fi
+
+# Copy .Xresources to destination directory with backup if file already exists
+if [ -f "$XRESOURCES_DEST/$XRESOURCES_FILE" ]; then
+  mv "$XRESOURCES_DEST/$XRESOURCES_FILE" "$XRESOURCES_BACKUP"
+fi
+
+cp "$XRESOURCES_FILE" "$XRESOURCES_DEST/$XRESOURCES_FILE"
+
+# Create symlink for youtube-watch.sh and make it executable
+ln -sf "$YOUTUBE_SCRIPT_SRC" "$YOUTUBE_SYMLINK_DEST"
+chmod +x "$YOUTUBE_SYMLINK_DEST"
+
+# Install required packages
+sudo pacman -Sy --noconfirm git terminator python polkit dunst i3 thunar thunar-archive-plugin thunar-media-tags-plugin thunar-volman nitrogen polybar ranger redshift mpv ffmpegthumbnailer xdotool urxvt rofi dmenu jq udisks2
+
+# Check if yay is installed
+if ! command -v yay &> /dev/null
+then
+    # Install yay if it's not installed
+    cd
+    sudo pacman -S --needed base-devel --noconfirm
+    git clone https://aur.archlinux.org/yay-bin.git
+    cd yay-bin
+    makepkg -si
+    cd ..
+    rm -rf yay-bin
+fi
+
+# Install additional packages
+yay -S --noconfirm Polybar networkmanager-dmenu-git pulseaudio-control drun3
+
+# Create .config directory if it doesn't exist
+if [ ! -d "$CONFIG_DIR" ]; then
+    mkdir -p "$CONFIG_DIR"
+fi
+
+# Copy contents of i3wm-polybar directory to ~/.config directory
+cp -r i3wm-polybar/* "$CONFIG_DIR/"
+
+# Additional instructions
+echo "+------------------------------------------------------------+"
+echo "|                                                            |"
+echo "|       Congratulations! i3wm-polybar setup completed.       |"
+echo "|                                                            |"
+echo "+------------------------------------------------------------+"
+echo ""
+echo "Please perform the following additional steps:"
+echo ""
+echo "1. Check the system-fonts.zip files, please install manually, you can download it on the internet."
+echo -e "\033[1m   I'm using Source Code Pro in `.Xresources` file, you might want to install it or change it.\033[0m"
+echo ""
+echo "2. Modify pulse config /etc/pulse/default.pa and add status false (if you want to use pulseaudio-control module)."
+echo "   load-module module-stream-restore [restore-device=false]"
+echo ""
+echo "3. In ~/.config/redshift.conf file, don't forget to change the latitude & longitude value."
+echo ""
+echo "4. Auto detect mounted & not mounted removable devices."
+echo "   - Create a new file /etc/udev/rules.d/95-usb.rules."
+echo "   - Add the following lines to the file:"
+echo "     KERNEL==\"sd*\", ACTION==\"add\", ATTR{removable}==\"1\", \\"
+echo "         RUN+=\"$POLYBAR_SCRIPTS_DIR/system-usb-udev.sh --update\""
+echo "     KERNEL==\"sd*\", ACTION==\"remove\", \\"
+echo "         RUN+=\"$POLYBAR_SCRIPTS_DIR/system-usb-udev.sh --update\""
+echo ""
+echo "5. Enabling touchpad tap to click."
+echo "   - Create a new file /etc/X11/xorg.conf.d/90-touchpad.conf."
+echo "   - Add the following lines to the file:"
+echo "     Section \"InputClass\""
+echo "             Identifier \"touchpad\""
+echo "             MatchIsTouchpad \"on\""
+echo "             Driver \"libinput\""
+echo "             Option \"Tapping\" \"on\""
+echo "     EndSection"
+echo ""
+echo "6. Change the wallpaper from nitrogen config file, restart Laptop/Computer, select i3 from display manager. That's it."
+echo ""
+echo "Thank you for using this setup script. Enjoy your i3wm-polybar setup!"
+echo ""
+
+echo "Done."
