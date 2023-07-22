30c33857bc33b0bf3949ec7b6b93bb4c92fc4616
Muh Ibnu Habil Hanafi <ibnu.habil.h@gmail.com>
2023-07-22 12:25:15 +0700

    Update extend_polybar script & tmux config

--- i3wm-polybar/polybar/extend_polybar.sh
+++ i3wm-polybar/polybar/extend_polybar.sh
@@ -4,10 +4,12 @@
 connected_monitors=$(polybar --list-monitors | wc -l)
 
 if [ "$connected_monitors" -gt 1 ]; then
-    for m in $(polybar --list-monitors | cut -d":" -f1); do
-		pkill polybar
+	killall -q polybar
 
-		sleep 2;
+	# Wait until the processes have been shut down
+	while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
+    
+    for m in $(polybar --list-monitors | cut -d":" -f1); do
         MONITOR=$m polybar --reload i3wm_bar &
     done
 fi
