07bb05cbaba4c0b23da559d3bc3b4e386e211aef
Muh Ibnu Habil Hanafi <ibnu.habil.h@gmail.com>
2023-11-23 11:28:28 +0700

    Update vim formatting to support async rust code

--- vim/lua/lsp-config/null-ls.lua
+++ vim/lua/lsp-config/null-ls.lua
@@ -11,7 +11,23 @@ local sources = {
 	-- formatting.stylelint, (for css etc.)
 	formatting.stylua,
 	--formatting.prettier, -- use it for json & markdown
-	formatting.rustfmt,
+	formatting.rustfmt.with({
+	    extra_args = function(params)
+	        local Path = require("plenary.path")
+	        local cargo_toml = Path:new(params.root .. "/" .. "Cargo.toml")
+
+	        if cargo_toml:exists() and cargo_toml:is_file() then
+	            for _, line in ipairs(cargo_toml:readlines()) do
+	                local edition = line:match([[^edition%s*=%s*%"(%d+)%"]])
+	                if edition then
+	                    return { "--edition=" .. edition }
+	                end
+	            end
+	        end
+	        -- default edition when we don't find `Cargo.toml` or the `edition` in it.
+	        return { "--edition=2021" }
+	    end,
+	}),
 	formatting.taplo,
 
 	--[[ code actions ]]
